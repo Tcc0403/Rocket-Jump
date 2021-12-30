ba7fb7b215ee0f6828a1c576ed6844cee051b6ae
Tcc0403 <76503978+Tcc0403@users.noreply.github.com>
2021-12-30 19:04:49 +0800

    Add trigger planes and modify physic materials
    Start plane: trigger Player.Save()
    Reset plane: trigger Player.Load()
    Finish plane: not done yet
    
    To set trigger, remember to add related trigger component on planes, and check convex, isTrigger in collider component.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public int currentStage;
     public void Save()
     {
+        currentStage = SceneManager.GetActiveScene().buildIndex;
         SaveSystem.SaveData(this);
         Debug.Log("Save data");
     }
@@ -15,7 +17,12 @@ public class Player : MonoBehaviour
         Debug.Log("Load data");
         PlayerData playerData = SaveSystem.LoadData();
         currentStage = playerData.currentStage;
-
+        if(currentStage == SceneManager.GetActiveScene().buildIndex)
+            LoadPosition(playerData);
+        
+    }
+    void LoadPosition(PlayerData playerData)
+    {
         Vector3 position;
         position.x = playerData.position[0]; 
         position.y = playerData.position[1]; 
