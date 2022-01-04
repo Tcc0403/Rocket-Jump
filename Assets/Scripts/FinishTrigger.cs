4b1da5977494259e3e53e8224ec869491ff0cd07
f74082052 <93573087+f74082052@users.noreply.github.com>
2022-01-04 18:10:22 +0800

    Next Scene

--- Assets/Scripts/FinishTrigger.cs
+++ Assets/Scripts/FinishTrigger.cs
@@ -5,25 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class FinishTrigger : MonoBehaviour
 {
-    
+    public GameObject gameObject;
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
     private void OnTriggerEnter(Collider other) {
         if(other.CompareTag("Player"))
         {
             Debug.Log("Goal");
-            Scene sceneLoaded = SceneManager.GetActiveScene();
-            if(sceneLoaded.buildIndex == 4)
-            {
-                SceneManager.LoadScene(0);
-            }
-            if (sceneLoaded.buildIndex == 0)
-            {
-                SceneManager.LoadScene(0);
-            }
-            else
-            {
-                SceneManager.LoadScene(sceneLoaded.buildIndex + 1);
-            }
-            
+            gameObject.SetActive(true);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
         }
+    }
+
+    public void Next()
+    {
+        Scene sceneLoaded = SceneManager.GetActiveScene();
+        if(sceneLoaded.buildIndex == 4)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneLoaded.buildIndex + 1);
+        }
+        
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
